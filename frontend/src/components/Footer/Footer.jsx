27d8ea18a0d8a539f7602ae907bf8557ae784f25
João Gabriel <jg.lofiego@gmail.com>
2022-06-13 22:44:58 -0300

    style: organiza elementos de acordo com mockup da parte desktop e adiciona link pra site da info

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,38 +8,44 @@ import instagram from "../../assets/instagram.svg";
 function Footer() {
   return (
     <Style>
-      <img src={logo} alt="Logo da lojinha" />
-      <nav>
-        <ul>
-          <li>
-            <a href="">Início</a>
-          </li>
-          <li>
-            <a href="">Produtos</a>
-          </li>
-          <li>
-            <a href="">Anunciar</a>
-          </li>
-          <li>
-            <a href="">Carrinho</a>
-          </li>
-        </ul>
-      </nav>
-      <nav>
+      <div className="top-level-footer">
         <button>
-          <img src={facebook} alt="Logo do facebook" />
+          <img src={logo} alt="Logo da lojinha" />
         </button>
-        <button>
-          <img src={twitter} alt="Logo do twitter" />
-        </button>
-        <button>
-          <img src={instagram} alt="Logo do instagram" />
-        </button>
-      </nav>
-      <p>
+        <nav>
+          <ul className="nav-links">
+            <li className="nav-link">
+              <a href="">Início</a>
+            </li>
+            <li className="nav-link">
+              <a href="">Produtos</a>
+            </li>
+            <li className="nav-link">
+              <a href="">Anunciar</a>
+            </li>
+            <li className="nav-link">
+              <a href="">Carrinho</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="link-buttons">
+          <button>
+            <img src={facebook} alt="Logo do facebook" />
+          </button>
+          <button>
+            <img src={twitter} alt="Logo do twitter" />
+          </button>
+          <button>
+            <img src={instagram} alt="Logo do instagram" />
+          </button>
+        </nav>
+      </div>
+      <p className="footer-message">
         Feito pela{" "}
         <span>
-          <a href="">Infojr UFBA</a>
+          <a className="infojr-link" href="https://infojr.com.br/" target="_blank">
+            Infojr UFBA
+          </a>
         </span>{" "}
         com Figma, React e muito 💚
       </p>
--- frontend/src/components/Footer/style.js
+++ frontend/src/components/Footer/style.js
@@ -2,6 +2,48 @@ import styled from "styled-components";
 
 const Style = styled.footer`
   background-color: #f5f5dc;
+  padding: 56px 120px 24px 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 100px;
+
+  button {
+    cursor: pointer;
+  }
+
+  .top-level-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .infojr-link {
+    text-decoration: underline;
+  }
+
+  .nav-links {
+    display: flex;
+    gap: 40px;
+  }
+
+  .nav-link {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  .link-buttons {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+  }
+
+  .footer-message {
+    font-size: 16px;
+    font-weight: 400;
+  }
 `;
 
 export default Style;
