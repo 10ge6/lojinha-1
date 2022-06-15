ce3b2c4561b7f99160fd60df32eecc240c675074
Clara <ferreira.clara22@hotmail.com>
2022-06-15 14:58:00 -0300

    desktop ok, terminando responsivo

--- frontend/src/components/Hero/index.jsx
+++ frontend/src/components/Hero/index.jsx
@@ -1,15 +1,20 @@
 import compras from '../../assets/compras.svg'
 import seta from '../../assets/Seta.svg'
+import * as S from './styles'
 
 function Hero() {
     return (
-        <div>
-            <p>Explore nossa nova coleção</p>
-            <p>Aproveite as ofertas e encontre o look ideal para aproveitar o seu São João na Lojinha.</p>
-            <p>Ver descontos</p>
-            <button><img src={seta} alt="seta pra baixo para redirecionamento"/></button>
+        <S.Hero>
+            <S.Text>
+                <h1>Explore nossa nova coleção</h1>
+                <p align='justify'>Aproveite as ofertas e encontre o look ideal para aproveitar o seu São João na Lojinha.</p>
+                <S.Discount>
+                    <p>Ver descontos</p>
+                    <button><img src={seta} alt="seta pra baixo para redirecionamento"/></button>
+                </S.Discount>
+            </S.Text>
             <img src={compras} alt="sacola de compras, presente, caixa para sapato com sapato em cima, etiqueta de preços e algumas folhas para decoração"/>
-        </div>
+        </S.Hero>
     )
 }
 
