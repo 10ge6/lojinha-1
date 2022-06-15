91b81480190735a52d134ff30471646a350c8aa7
João Gabriel <jg.lofiego@gmail.com>
2022-06-15 18:48:31 -0300

    feat: adiciona todos os elementos dos filtros

--- frontend/src/components/ProductFilter/index.jsx
+++ frontend/src/components/ProductFilter/index.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import vector from "../../assets/VectorRight.svg";
+import camisa from "../../assets/camisa.png";
+import tenis from "../../assets/tenis.png";
+import bolsa from "../../assets/bolsa.png";
 import * as S from "./styles";
 
 function ProductFilter() {
-  return <div></div>;
+  return (
+    <S.Section>
+      <div>
+        <img src={camisa} alt="Imagem de uma camisa" />
+        <h2>Camisas</h2>
+        <button>
+          <p>Ver mais</p>
+          <img src={vector} alt="Botão de Ver mais" />
+        </button>
+      </div>
+      <div>
+        <img src={tenis} alt="Imagem de uma camisa" />
+        <h2>Tênis</h2>
+        <button>
+          <p>Ver mais</p>
+          <img src={vector} alt="Botão de Ver mais" />
+        </button>
+      </div>
+      <div>
+        <img src={bolsa} alt="Imagem de uma camisa" />
+        <h2>Bolsas</h2>
+        <button>
+          <p>Ver mais</p>
+          <img src={vector} alt="Botão de Ver mais" />
+        </button>
+      </div>
+    </S.Section>
+  );
 }
 
 export default ProductFilter;
--- frontend/src/components/ProductFilter/styles.js
+++ frontend/src/components/ProductFilter/styles.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  max-width: 1680px;
+  margin: 0 auto;
+`;
