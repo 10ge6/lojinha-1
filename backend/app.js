293c71ff8198cd5499d78088ac97946a33fb01aa
10ge6 <dsf@jugemu.systems>
2022-06-16 17:48:59 -0300

    setup express + mysql2 + CRUD do storefront

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+app.use(express.static('../frontend/dist'));
+
+const morgan = require('morgan');
+
+// setup de body
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// logs
+app.use(morgan('dev'));
+
+// CORS
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		);
+	
+	if(req.method == 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+		return res.status(200).send({});
+	}
+	
+	next();
+	
+});
+
+// rotas
+const storefrontController = require('./routes/storefront/controller');
+// const cartController = require('./routes/cart/controller');
+
+// endpoint
+app.use('/storefront', storefrontController);
+// app.use('/cart', cartController);
+
+// fallback
+app.use((error, res) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            message: error.message
+        }
+    });
+});
+
+
+module.exports = app;
---
+++ backend/routes/storefront/service.js
@@ -0,0 +1,129 @@
+const sql = require('../../db').pool;
+
+// CRUD functions
+
+// criando item
+exports.createItem = (req, res) => {
+    sql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'INSERT INTO storefront (product_pic, product_title, product_brand, product_color, product_category, product_subcategory, product_price, product_size) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [req.body.product_pic, req.body.product_title,
+            req.body.product_brand, req.body.product_color, req.body.product_category,
+            req.body.product_subcategory, req.body.product_price, req.body.product_size],
+            (error) => {
+                conn.release();
+
+                if(error) {
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+
+                res.status(201).send({
+                    message: 'Item criado com sucesso'
+                });
+            }
+        );
+    });
+}
+
+// retornando todos os itens da loja
+exports.readStorefront = (req, res) => {
+    sql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM storefront',
+            (error, response) => {
+                if(error) {
+                    conn.release();
+
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+
+                res.status(200).send({
+                    message: 'Exibindo todos os itens',
+                    response: response
+                });
+            }
+        );
+    });
+}
+
+// retornando item especifico da loja
+exports.readItem = (req, res) => {
+    sql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM storefront WHERE product_id = ?',
+            [req.params.product_id],
+            (error, response) => {
+                if(error) {
+                    conn.release();
+
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+                
+                res.status(200).send({
+                    message: 'Exibindo infos do user',
+                    response: response
+                });
+            }
+        );
+    });
+}
+
+// atualizando item da loja
+exports.updateItem = (req, res) => {
+    sql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'UPDATE storefront SET product_pic = ?, product_title = ?, product_brand = ?, product_color = ?, product_category = ?, product_subcategory = ?, product_price = ?, product_size = ? WHERE product_id = ?',
+            [req.body.product_pic, req.body.product_title, req.body.product_brand,
+            req.body.product_color, req.body.product_category, req.body.product_subcategory,
+            req.body.product_price, req.body.product_size, req.params.product_id],
+            (error) => {
+                conn.release();
+
+                if(error) {
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+
+                res.status(200).send({
+                    message: 'Item atualizado com sucesso'
+                });
+            }
+        );
+    });
+}
+
+// deletando item da loja
+exports.deleteItem = (req, res) => {
+    sql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'DELETE FROM storefront WHERE product_id = ?',
+            [req.params.product_id],
+            (error, response) => {
+                if(error) {
+                    conn.release();
+
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+
+                res.status(200).send({
+                    message: 'Item deletado',
+                    response: response
+                });
+            }
+        );
+    });
+}
