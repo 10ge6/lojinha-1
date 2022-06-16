be8ec3067e68d51117e58e6e6a52b505ae99c5ff
João Gabriel <jg.lofiego@gmail.com>
2022-06-16 20:09:34 -0300

    feat: adiciona arquivos da secao de produtos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import ProductFilter from "./components/ProductFilter";
 import GlobalStyle from "./styles/Global";
+import ProductSection from "./components/ProductSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <ProductFilter />
+      <ProductSection />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/ProductSection/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Section} from "../../styles/Global";
+
+function ProductSection() {
+  return <Section></Section>;
+}
+
+export default ProductSection;
