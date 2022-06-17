92b5f795d325d7aaf9a82dea1b713cf989b68a22
João Gabriel <jg.lofiego@gmail.com>
2022-06-16 23:28:22 -0300

    feat: troca a porta do server pra 8000 e consegue dar fetch nos produtos

--- frontend/src/components/ProductSection/index.jsx
+++ frontend/src/components/ProductSection/index.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import {Section} from "../../styles/Global";
+import {useState, useEffect} from "react";
+import * as S from "./styles";
 
 function ProductSection() {
-  return <Section></Section>;
+  const [users, setUsers] = useState([]);
+  async function getUsers() {
+    const response = await fetch("http://localhost:8000/storefront");
+    const data = await response.json();
+    return data.response;
+  }
+
+  useEffect(() => {
+    getUsers().then((users) => {
+      console.log(users);
+      setUsers(users);
+    });
+  }, []);
+
+  return (
+    <Section>
+      <div>
+        {users.map((user) => (
+          <div>{console.log(user)}</div>
+        ))}
+      </div>
+    </Section>
+  );
 }
 
 export default ProductSection;
--- frontend/src/components/ProductSection/styles.js
+++ frontend/src/components/ProductSection/styles.js
@@ -1,1 +1,6 @@
 import styled from "styled-components";
+
+const ProductList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
