6dc0650340463171175e1514a34694cde6e2a1d9
João Gabriel <jg.lofiego@gmail.com>
2022-06-17 02:18:06 -0300

    feat: adiciona informacoes dos produtos tirando os tamanhos

--- frontend/src/components/ProductSection/index.jsx
+++ frontend/src/components/ProductSection/index.jsx
@@ -11,9 +11,16 @@ function ProductSection() {
     return data.response;
   }
 
+  function textPrice(price) {
+    let text = price.toString();
+    if (Number.isInteger(price / 100)) {
+      return <p>{"R$ " + text.slice(0, text.length - 2)}</p>;
+    }
+    return <p>{"R$ " + text.slice(0, text.length - 2) + "," + text.slice(text.length - 2, text.length)}</p>;
+  }
+
   useEffect(() => {
     getUsers().then((users) => {
-      console.log(users);
       setUsers(users);
     });
   }, []);
@@ -22,7 +29,18 @@ function ProductSection() {
     <Section>
       <div>
         {users.map((user) => (
-          <div>{console.log(user)}</div>
+          <div key={user.product_id}>
+            <img
+              src={user.product_pic}
+              onError={({currentTarget}) => {
+                currentTarget.src = "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png";
+              }}
+              alt="Imagem do produto"
+            />
+            {textPrice(user.product_price)}
+            <p>{user.product_title}</p>
+            <p>{user.product_brand}</p>
+          </div>
         ))}
       </div>
     </Section>
