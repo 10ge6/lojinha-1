e1215419c90934cac5047b44865041769d8d9d92
João Gabriel <jg.lofiego@gmail.com>
2022-06-18 12:38:05 -0300

    feat: adiciona os elementos da pagina

--- frontend/src/components/PageNotFound/index.jsx
+++ frontend/src/components/PageNotFound/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import {Section} from "../../styles/Global";
+import {Link} from "react-router-dom";
 
 function PageNotFound() {
   return (
     <Section>
-      <div></div>
+      <div>
+        <h1>404: Página Não Encontrada :(</h1>
+        <h2>Infelizmente, sua requisição não pode ser concluída</h2>
+        <p>
+          Para retornar à página principal{" "}
+          <Link className="link-home" to="/">
+            clique aqui
+          </Link>{" "}
+          ou utilize os links no topo ou fim dessa página
+        </p>
+      </div>
     </Section>
   );
 }
