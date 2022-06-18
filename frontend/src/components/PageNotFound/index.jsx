f2be4429c735ebb9a9cc779c5d2e11fb544e192e
João Gabriel <jg.lofiego@gmail.com>
2022-06-18 12:07:46 -0300

    feat: adiciona componente da pagina e linka ele a pagina de erro

---
+++ frontend/src/components/PageNotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Section} from "../../styles/Global";
+
+function PageNotFound() {
+  return (
+    <Section>
+      <div></div>
+    </Section>
+  );
+}
+
+export default PageNotFound;
