04e1f58cb0b3e41bfaa6fbb5a4474eca358e85a2
João Gabriel <jg.lofiego@gmail.com>
2022-06-20 16:32:39 -0300

    style: estiliza os elementos existentes como no mockup do desktop

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -24,34 +24,34 @@ function Modal({ url }) {
          return (
             <S.ModalBg>
                <S.Modal>
-                  <div>
-                     <h1>{modalData.product_title}</h1>
-                     <S.Button onClick={() => setModal(false)}>
+                  <S.TopSideModal>
+                     <S.Title>{modalData.product_title}</S.Title>
+                     <S.CloseBtn onClick={() => setModal(false)}>
                         <img src={closeBtn} alt='Botão de fechar modal' />
-                     </S.Button>
-                  </div>
-                  <div>
+                     </S.CloseBtn>
+                  </S.TopSideModal>
+                  <S.Content>
+                     <S.Categories>
+                        <S.Category>
+                           <S.CategoryTitle>Descrição</S.CategoryTitle>
+                           <S.CategoryContent>{modalData.product_desc}</S.CategoryContent>
+                        </S.Category>
+                        <S.Category>
+                           <S.CategoryTitle>Vendedor</S.CategoryTitle>
+                           <S.CategoryContent>RickRoll</S.CategoryContent>
+                        </S.Category>
+                        <S.Category>
+                           <S.CategoryTitle>Marca</S.CategoryTitle>
+                           <S.CategoryContent>{modalData.product_brand}</S.CategoryContent>
+                        </S.Category>
+                        <S.Category>
+                           <S.CategoryTitle>Cor</S.CategoryTitle>
+                           <S.CategoryContent>{modalData.product_color}</S.CategoryContent>
+                        </S.Category>
+                        <S.Price>{(modalData.product_price / 100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</S.Price>
+                     </S.Categories>
                      <div>
-                        <div>
-                           <h2>Descrição</h2>
-                           <p>{modalData.product_desc}</p>
-                        </div>
-                        <div>
-                           <h2>Vendedor</h2>
-                           <p>RickRoll</p>
-                        </div>
-                        <div>
-                           <h2>Marca</h2>
-                           <p>{modalData.product_brand}</p>
-                        </div>
-                        <div>
-                           <h2>Cor</h2>
-                           <p>{modalData.product_color}</p>
-                        </div>
-                        <h2>{(modalData.product_price / 100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h2>
-                     </div>
-                     <div>
-                        <img
+                        <S.Img
                            src={modalData.product_pic}
                            onError={({ currentTarget }) => {
                               currentTarget.src = 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png';
@@ -59,7 +59,7 @@ function Modal({ url }) {
                            alt='Imagem do produto'
                         />
                      </div>
-                  </div>
+                  </S.Content>
                </S.Modal>
             </S.ModalBg>
          );
--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -20,8 +20,72 @@ export const ModalBg = styled.div`
 `;
 
 export const Modal = styled.div`
+   display: flex;
+   flex-direction: column;
    width: 60%;
-   height: 80%;
-   padding: 100px 112px;
+   height: 90%;
+   padding: 50px 66px;
+   border-radius: 20px;
    background-color: #fafafa;
+`;
+
+export const TopSideModal = styled.div`
+   display: grid;
+   grid-template-columns: 30px 1fr 30px;
+`;
+
+export const CloseBtn = styled.button`
+   cursor: pointer;
+   grid-column: 3;
+   justify-self: center;
+`;
+
+export const Title = styled.h1`
+   font-family: 'Montserrat';
+   font-size: 36px;
+   color: #252822;
+   font-weight: 500;
+   grid-column: 2;
+   justify-self: center;
+`;
+
+export const Content = styled.div`
+   display: flex;
+   justify-content: space-between;
+   gap: 100px;
+`;
+
+export const Categories = styled.div`
+   display: flex;
+   flex-direction: column;
+   gap: 16px;
+`;
+
+export const Category = styled.div`
+   display: flex;
+   flex-direction: column;
+   gap: 8px;
+`;
+
+export const CategoryTitle = styled.h2`
+   font-weight: 600;
+   font-size: 30px;
+`;
+
+export const CategoryContent = styled.p`
+   font-weight: 400;
+   font-size: 20px;
+   font-family: 'Montserrat';
+`;
+
+export const Price = styled.h2`
+   font-weight: 600;
+   font-size: 40px;
+   color: #275845;
+`;
+
+export const Img = styled.img`
+   width: 18vw;
+   height: 18vw;
+   object-fit: cover;
 `;
