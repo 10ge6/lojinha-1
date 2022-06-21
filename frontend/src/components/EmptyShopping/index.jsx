6711b4fc5b531bc7e0b66606f5c83232d9f5c25f
Clara <ferreira.clara22@hotmail.com>
2022-06-21 20:38:33 -0300

    desktop e responsivo ok

--- frontend/src/components/EmptyShopping/index.jsx
+++ frontend/src/components/EmptyShopping/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import * as S from './styles'
-import * as S from '../ProductSection/styles'
+import * as S from './styles' 
 import setinha from '../../assets/setinha.svg'
 import image from '../../assets/image.svg'
 
@@ -12,19 +11,27 @@ function EmptyShopping () {
                     <img src={setinha} alt="seta"/>
                     <h3>Carrinho</h3>
                 </S.Title>
-                <S.Text>
-                    <h1>Seu carrinho está vazio :(</h1>
-                    <h2><a>Continue navegando</a> pela Lojinha e encontre produtos incríveis!</h2>
-                </S.Text>
+                <S.ContainerText>
+                    <S.Text>
+                        <h1>Seu carrinho está vazio :(</h1>
+                        <h2><a>Continue navegando</a> pela Lojinha e encontre produtos incríveis!</h2>
+                    </S.Text>
+                </S.ContainerText>
             </S.Section>
             <S.Article>
                 <h3>Visto recentemente</h3>
-                <S.ProductCard>
-                    <S.ProductImg><img src={image}/></S.ProductImg>
-                    <S.ProductSizeDiv><S.ProductSize><p>M</p></S.ProductSize><S.ProductSize><p>G</p></S.ProductSize></S.ProductSizeDiv>
-                    <S.ProductTitle><p>camisa social polo original</p></S.ProductTitle>
-                    <S.ProductBrand><p>polo</p></S.ProductBrand>
-                </S.ProductCard>  
+                <S.ContainerProduct>
+                    <S.image>
+                        <img src={image} alt="homem vestindo uma camisa azul, de manga comprida, da marca polo"/> 
+                    </S.image>
+                    <S.ContainerSize>
+                        <S.Size><p>M</p></S.Size>
+                        <S.Size><p>G</p></S.Size>
+                    </S.ContainerSize> 
+                    <S.Price><p>R$ 54</p></S.Price>
+                    <S.Product><p>camisa social polo original</p></S.Product>
+                    <S.Brand><p>polo</p></S.Brand>  
+                </S.ContainerProduct>   
             </S.Article>
         </S.Shopping>
     )
--- frontend/src/components/EmptyShopping/styles.js
+++ frontend/src/components/EmptyShopping/styles.js
@@ -2,22 +2,53 @@ import styled from 'styled-components'
 
 export const Shopping = styled.div `
     display: flex;
+    justify-content: space-around;  
+    padding: 100px 0 100px 0;
+
+    @media (max-width: 960px) {
+        display: flex;
+        flex-direction: column;
+
+        align-items: center;
+        gap: 100px;
+    }
 `;
 
 export const Section = styled.div `
     display: flex;
     flex-direction: column;
+    width: 60%;
+
+    @media (max-width: 960px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        gap: 60px
+    }
+
+    @media (max-width: 750px) {
+        width: 90%;
+    }
 `;
 
 export const Title = styled.div `
   display: flex;
   align-items: center;
-  
+  gap: 5px;
+
   h3 {
     color: #275845;
     font-weight: 600;
     font-size: 1.375rem;
   }
+`;
+export const ContainerText = styled.div `
+    display: flex;
+    justify-content: flex-end;
+    height: 100%;
+    align-items: center;
+
 `;
 
 export const Text = styled.div `
@@ -35,11 +66,21 @@ export const Text = styled.div `
         font-weight: 500;
         font-size: 1.375rem;
     }
+
+    @media (max-width: 960px) {
+        display: flex;
+        align-items: center;
+        text-align: center;
+        gap: 30px;
+    }
+
+
 `;
 
 export const Article = styled.div `
     display: flex;
     flex-direction: column;
+    
     gap: 46px;
 
     h3 {
@@ -47,4 +88,60 @@ export const Article = styled.div `
         font-weight: 600;
         font-size: 1.125rem;
     }
+
+    @media (max-width: 960px) {
+        display: flex;
+        align-items: center;
+
+        h3 {
+            font-size: 1.5rem;
+        }
+    }
+`;
+
+export const ContainerProduct = styled.div `
+`;
+
+export const image = styled.div `
+    width: 247px;
+    height: 244px;
+
+    img {
+        object-fit: cover;
+    }
+`;
+export const ContainerSize = styled.div `
+    display: flex;
+    margin: -32px 0 0 2px;
+    gap: 2px;
+`;
+export const Size = styled.div `
+    align-items: center;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+    font-size: 0.75rem;
+    background-color: white;
+    padding: 6px 10px;
+    border-radius: 2px;
+`;
+export const Price = styled.div `
+    color: #53B38D;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    font-size: 1.25rem;
+
+`;
+export const Product = styled.div `
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    color: #252822;
+    font-size: 1.125rem;
+
+`;
+export const Brand = styled.div `
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    color: #A6A39F;
+    font-size: 1.125rem;
+
 `;
