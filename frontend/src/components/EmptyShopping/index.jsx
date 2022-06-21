570c1261c8177088c5842aacc934883e947000c2
Clara <ferreira.clara22@hotmail.com>
2022-06-21 17:50:17 -0300

    adicionei os elementos e importei o styles de ProductSection

--- frontend/src/components/EmptyShopping/index.jsx
+++ frontend/src/components/EmptyShopping/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import * as S from './styles'
+import * as S from '../ProductSection/styles'
+import setinha from '../../assets/setinha.svg'
 
 function EmptyShopping () {
     return (
-       <S.Section></S.Section> 
+        <div>
+            <S.Section>
+                <img src={setinha} alt="seta"/>
+                <h3>Carrinho</h3>
+                <S.Text>
+                <h1>Seu carrinho está vazio :(</h1>
+                <h2>Continue navegando pela Lojinha e encontre produtos incríveis!</h2>
+                </S.Text>
+            </S.Section>
+            <S.Article>
+                <S.ProductImg></S.ProductImg>
+                <S.ProductSize></S.ProductSize>
+                <S.ProductTitle></S.ProductTitle>
+                <S.ProductBrand></S.ProductBrand>
+            </S.Article>
+        </div>
     )
 }
 
--- frontend/src/components/EmptyShopping/styles.js
+++ frontend/src/components/EmptyShopping/styles.js
@@ -3,3 +3,11 @@ import styled from 'styled-components'
 export const Section = styled.div `
 
 `;
+
+export const Text = styled.div `
+
+`;
+
+export const Article = styled.div `
+
+`;
