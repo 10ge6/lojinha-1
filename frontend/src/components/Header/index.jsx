2e92400398d29666d9d7903fdab2b0d82f0cf938
João Gabriel <jg.lofiego@gmail.com>
2022-06-22 17:18:13 -0300

    feat: adiciona funcao de scroll lock no menu hamburguer

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,58 +3,65 @@ import lupaPesquisa from '../../assets/lupa.svg';
 import menu from '../../assets/hamburguer.svg';
 import X from '../../assets/X.svg';
 import * as S from './styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Section } from '../../styles/Global';
 import { Link } from 'react-router-dom';
+import { scrollLock } from '../Modal';
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+   const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
-    <S.Wrapper className={menuOpen ? 'open' : 'closed'}>
-      <Section>
-        <S.Header>
-          <S.Logo>
-            <img src={logoLojinha} alt='logo da lojinha' />
-            <h1>Lojinha</h1>
-          </S.Logo>
-          <S.Search className={menuOpen ? 'open' : 'closed'}>
-            <input type='text' placeholder='Pesquisar por um produto' />
-            <button>
-              <img src={lupaPesquisa} alt='lupa para pesquisa' />
-            </button>
-          </S.Search>
-          <S.Nav className={menuOpen ? 'open' : 'closed'}>
-            <ul>
-              <li>
-                <Link onClick={() => setMenuOpen(false)} to='/'>
-                  Início
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => setMenuOpen(false)} to='/products'>
-                  Produtos
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => setMenuOpen(false)} to='/announce'>
-                  Anunciar
-                </Link>
-              </li>
-              <li className='carrinho'>
-                <Link onClick={() => setMenuOpen(false)} to='/shopping'>
-                  Carrinho
-                </Link>
-              </li>
-            </ul>
-          </S.Nav>
-          <S.Menu>
-            <button onClick={() => setMenuOpen((state) => !state)}>{menuOpen ? <img src={X} /> : <img src={menu} />}</button>
-          </S.Menu>
-        </S.Header>
-      </Section>
-    </S.Wrapper>
-  );
+   useEffect(() => {
+      scrollLock(menuOpen);
+   }, [menuOpen]);
+
+   return (
+      <S.Wrapper className={menuOpen ? 'open' : 'closed'}>
+         <Section>
+            <S.Header>
+               <S.Logo>
+                  <img src={logoLojinha} alt='logo da lojinha' />
+                  <h1>Lojinha</h1>
+               </S.Logo>
+               <S.Search className={menuOpen ? 'open' : 'closed'}>
+                  <input type='text' placeholder='Pesquisar por um produto' />
+                  <button>
+                     <img src={lupaPesquisa} alt='lupa para pesquisa' />
+                  </button>
+               </S.Search>
+               <S.Nav className={menuOpen ? 'open' : 'closed'}>
+                  <ul>
+                     <li>
+                        <Link onClick={() => setMenuOpen(false)} to='/'>
+                           Início
+                        </Link>
+                     </li>
+                     <li>
+                        <Link onClick={() => setMenuOpen(false)} to='/products'>
+                           Produtos
+                        </Link>
+                     </li>
+                     <li>
+                        <Link onClick={() => setMenuOpen(false)} to='/announce'>
+                           Anunciar
+                        </Link>
+                     </li>
+                     <li className='carrinho'>
+                        <Link onClick={() => setMenuOpen(false)} to='/shopping'>
+                           Carrinho
+                        </Link>
+                     </li>
+                  </ul>
+               </S.Nav>
+               <S.Menu>
+                  <button onClick={() => setMenuOpen((state) => !state)}>
+                     {menuOpen ? <img src={X} /> : <img src={menu} />}
+                  </button>
+               </S.Menu>
+            </S.Header>
+         </Section>
+      </S.Wrapper>
+   );
 }
 
 export default Header;
