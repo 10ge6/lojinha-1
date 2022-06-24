d09eebbf334c2cb1ef99faf9ea85bee30fc5c882
Clara <ferreira.clara22@hotmail.com>
2022-06-24 18:58:18 -0300

    página de produtos ok

--- frontend/src/pages/Shopping.jsx
+++ frontend/src/pages/Shopping.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ShoppingCart from "../components/ShoppingCart";
+import EmptyShopping from "../components/EmptyShopping"
 import {useState, useEffect } from 'react'
 
 function Shopping() {
     
     const [datasCart, setDatasCart] = useState([])
     
-    
-    
+    useEffect(() => {
+        fetch('http://localhost:8000/cart', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setDatasCart(data.response)
+        })
+        .catch((err) => console.log(err))
+
+    }, [])
+
     return ( 
         <div>
-        <ShoppingCart/>
+            {datasCart.length > 0 ? <ShoppingCart/> : <EmptyShopping/> }
         </div>
     );
 }
